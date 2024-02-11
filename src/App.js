26bf1f49230cd936ec9b0a212eacd0282a0df46c
JuanIgnacioMarchetto <tu@email.com>
2024-02-11 09:49:02 -0300

    11/2/2024-09:48

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import GitHubProfile from './GitHubProfile';
+
+const App = () => {
+  const myGitHubUsername = 'JuanIgnacioMarchetto';
+
+  return (
+    <div>
+      <GitHubProfile username={myGitHubUsername} />
+    </div>
+  );
+};
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
+export default App;
---
+++ src/GitHubProfile.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+
+const GitHubProfile = ({ username }) => {
+    const [userData, setUserData] = useState(null);
+
+    useEffect(() => {
+        const fetchUserData = async () => {
+            try {
+                const response = await fetch(`https://api.github.com/users/${username}`);
+                if (response.ok) {
+                    const data = await response.json();
+                    setUserData(data);
+                } else {
+                    console.error('Error al obtener datos del usuario');
+                }
+            } catch (error) {
+                console.error('Error de red:', error);
+            }
+        };
+
+        fetchUserData();
+    }, [username]);
+
+    return (
+        <div>
+            <h1>Perfil de GitHub</h1>
+            {userData ? (
+                <div>
+                    <img src={userData.avatar_url} alt="Avatar" style={{ width: '100px', borderRadius: '50%' }} />
+                    <h2>{userData.name}</h2>
+                    <p>{userData.bio}</p>
+                    <p>Seguidores: {userData.followers}</p>
+                    <p>Repositorios públicos: {userData.public_repos}</p>
+                    <p>Sitio web: <a href={userData.blog}>{userData.blog}</a></p>
+                </div>
+            ) : (
+                <p>Cargando...</p>
+            )}
+        </div>
+    );
+};
+
+export default GitHubProfile;
